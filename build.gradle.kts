30ad6f96cb5e6fb77a1f63ff0ed9e476884684db
Tiago Paulin <127238987+TiagoPaulin@users.noreply.github.com>
2024-04-24 15:08:21 -0300

    configuração do gradle

---
+++ build.gradle.kts
@@ -0,0 +1,67 @@
+plugins {
+    alias(libs.plugins.androidApplication)
+    alias(libs.plugins.jetbrainsKotlinAndroid)
+}
+
+android {
+    namespace = "com.example.processamentodeimagem"
+    compileSdk = 34
+
+    defaultConfig {
+        applicationId = "com.example.processamentodeimagem"
+        minSdk = 34
+        targetSdk = 34
+        versionCode = 1
+        versionName = "1.0"
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+    kotlinOptions {
+        jvmTarget = "1.8"
+    }
+
+    // configurando binding
+    dataBinding{
+        enable = true
+    }
+    buildFeatures{
+        dataBinding = true
+        viewBinding = true
+    }
+
+}
+
+dependencies {
+
+    // bibliotec cameraX
+    implementation("androidx.camera:camera-camera2:1.1.0-alpha03")
+    implementation("androidx.camera:camera-lifecycle:1.1.0-alpha03")
+    implementation("androidx.camera:camera-view:1.0.0-alpha23")
+
+    // draw to bitmap
+    implementation("androidx.core:core-ktx:1.7.0")
+
+    implementation(libs.androidx.core.ktx)
+    implementation(libs.androidx.appcompat)
+    implementation(libs.material)
+    implementation(libs.androidx.activity)
+    implementation(libs.androidx.constraintlayout)
+    implementation(libs.cronet.embedded)
+    testImplementation(libs.junit)
+    androidTestImplementation(libs.androidx.junit)
+    androidTestImplementation(libs.androidx.espresso.core)
+}
